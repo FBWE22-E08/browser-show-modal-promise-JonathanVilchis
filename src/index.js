e982ae3b5f15a056d59296f45f87c1eb602190d5
Jonathan Vilchis <joco_va@hotmail.com>
2023-01-30 13:55:14 +0100

    show-modal

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import "./css/style.css"; //importing css
+
+let modal = document.querySelector(".modal");
+let close = document.querySelector(".close");
+
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve((modal.style.display = "block"));
+    }, 6000);
+});
+
+const resolving = async () => {
+    let result = await myPromise();
+    return result;
+};
+
+resolving();
+
+close.style.cursor = "pointer";
+
+close.addEventListener("click", function () {
+    modal.style.display = "none";
+});
+
+// const miPromesa = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve('La promesa se cumplió');
+//     }, 2000);
+//   });
